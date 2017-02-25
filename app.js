93612e985fd43d92dcbc22b9062b64e33b4e4221
Clinton Kunhardt <24also20@gmail.com>
2017-02-25 02:16:29 -0600

    pug + more

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
