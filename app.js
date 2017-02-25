5f8cb256b283711fab7dc475e2739b1eecd908fa
Clinton Kunhardt <24also20@gmail.com>
2017-02-25 04:47:39 -0600

    chat alpha

--- app.js
+++ app.js
@@ -13,17 +13,18 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// uncomment after placing your favicon in /views
+//app.use(favicon(path.join(__dirname, 'views', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 module.exports = app;
